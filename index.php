2be9a14dbec33708f7a55211213e7dc559a785e7
arffsaad <80538339+arffsaad@users.noreply.github.com>
2021-11-23 23:23:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 </head>
 
 <body>
+    <p>Welcome to our annual AD Defense Workshop! The purpose of our workshop is to equip System Administrators with the basics and even dive into some 
+        advanced defense techniques to safeguard your organization's AD environment. We will also share the industry standard for maintaining the environment, 
+        as well as the do's and dont's of becoming a Domain Admin.</p>
     
 </body>
 
