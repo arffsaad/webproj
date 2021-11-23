c706db1d68967b7262a460345e217e00924c096d
arffsaad <80538339+arffsaad@users.noreply.github.com>
2021-11-23 23:51:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>AD DEFENSE ANNUAL WORKSHOP</title>
+<html><head>
+    <title>AD DEFENSE ANNUAL WORKSHOP</title><style>
+    html, body{
+  background-color:#000000;
+  font-family: Arial, Helvetica, sans-serif;
+  height: 100%;
+}
+.top{
+  position:fixed;
+  width:100%;
+  height:50px;
+  background-color:#6cfff8;
+  top:0px;
+  left:0px;
+  z-index: 10;
+}
+</style>
 </head>
 
 <body>
-    <p>Welcome to our annual AD Defense Workshop! The purpose of our workshop is to equip System Administrators with the basics and even dive into some 
-        advanced defense techniques to safeguard your organization's AD environment. We will also share the industry standard for maintaining the environment, 
-        as well as the do's and dont's of becoming a Domain Admin.</p>
-    
-</body>
+    <div>
+    <div class="top">
+    </div>
 
-</html>
+<span style="position:relative"><p style="color:#ffffff; font-size: 1em;">Welcome to our annual AD Defense Workshop! The purpose of our workshop is to equip System Administrators with the basics and even dive into some 
+        advanced defense techniques to safeguard your organization's AD environment. We will also share the industry standard for maintaining the environment, 
+    as well as the do's and dont's of becoming a Domain Admin.</p></span>
+</div>
+    
+
+
+
+</body></html>
